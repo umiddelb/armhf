b6febdc5878b1b633970aba6c24e37d99e4f4dc8
Uli Middelberg <uli@middelberg.de>
2015-05-29 22:01:49 +0200

    replaced 'ubuntu' by $SUDO_USER

--- ubuntu-core/common/ubuntu-docker-00.sh
+++ ubuntu-core/common/ubuntu-docker-00.sh
@@ -4,4 +4,4 @@ sudo apt-get -y install lxc aufs-tools cgroup-lite apparmor docker.io
 sudo service docker.io stop
 sudo curl -sSL https://github.com/umiddelb/armhf/blob/master/bin/docker-1.6.0?raw=true >/usr/bin/docker
 sudo service docker.io start
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker $SUDO_USER
