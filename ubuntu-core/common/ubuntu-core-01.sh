b37fd884a75aeb51789c8be4c260c71453fffb54
Uli Middelberg <uli@middelberg.de>
2015-06-03 23:28:48 +0200

    noise reduction

--- ubuntu-core/common/ubuntu-core-01.sh
+++ ubuntu-core/common/ubuntu-core-01.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sSL http://cdimage.ubuntu.com/ubuntu-core/releases/14.04.2/release/ubuntu-core-14.04-core-armhf.tar.gz | sudo tar --numeric-owner -xpzvf - -C rootfs/
+curl -sSL http://cdimage.ubuntu.com/ubuntu-core/releases/14.04.2/release/ubuntu-core-14.04-core-armhf.tar.gz | sudo tar --numeric-owner -xpzf - -C rootfs/
 
 sudo mount -o bind /dev ./rootfs/dev
 sudo mount -o bind /dev/pts ./rootfs/dev/pts
@@ -12,3 +12,8 @@ sudo cp ubuntu-docker-00.sh ./rootfs
 
 sudo chroot ./rootfs sh /ubuntu-core-02.sh
 sudo rm ./rootfs/ubuntu-core-02.sh
+
+sudo umount ./rootfs/dev/pts
+sudo umount ./rootfs/sys
+sudo umount -l ./rootfs/dev
+sudo umount -l ./rootfs/proc
--- ubuntu-core/odroid-c1/ubuntu-core-02.sh
+++ ubuntu-core/odroid-c1/ubuntu-core-02.sh
@@ -18,23 +18,25 @@ echo "nameserver 8.8.8.8" > /etc/resolv.conf
 dpkg-divert --local --rename --add /sbin/initctl
 ln -s /bin/true /sbin/initctl
 
-apt-get update
-apt-get -y upgrade
-apt-get -y dist-upgrade
-apt-get -y install software-properties-common u-boot-tools isc-dhcp-client ubuntu-minimal ssh
-apt-get -y install cifs-utils screen wireless-tools iw curl libncurses5-dev cpufrequtils rcs aptitude make bc lzop man-db ntp usbutils pciutils lsof most sysfsutils linux-firmware linux-firmware-nonfree
+export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y install python-software-properties
+apt-get -q=2 update
+apt-get -q=2 -y upgrade
+apt-get -q=2 -y dist-upgrade
+apt-get -q=2 -y install software-properties-common u-boot-tools isc-dhcp-client ubuntu-minimal ssh
+apt-get -q=2 -y install cifs-utils screen wireless-tools iw curl libncurses5-dev cpufrequtils rcs aptitude make bc lzop man-db ntp usbutils pciutils lsof most sysfsutils linux-firmware linux-firmware-nonfree
+
+apt-get -q=2 -y install python-software-properties
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-get update
-apt-get -y install gcc
+apt-get -q=2 update
+apt-get -q=2 -y install gcc
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AB19BAC9
 echo "deb http://deb.odroid.in/c1/ trusty main" > /etc/apt/sources.list.d/odroid.list
 echo "deb http://deb.odroid.in/ trusty main" >> /etc/apt/sources.list.d/odroid.list
-apt-get update
+apt-get -q=2 update
 mkdir -p /media/boot
-apt-get -y install linux-image-c1 bootini
+apt-get -q=2 -y install linux-image-c1 bootini
 cp boot/uInitrd-3.10.* /media/boot/uInitrd 
 cp boot/uImage-3.10.* /media/boot/uImage  
 
@@ -50,6 +52,6 @@ echo "exec /sbin/getty -L 115200 ttyS0 vt102" >> /etc/init/ttyS0.conf
 adduser --gecos '' ubuntu
 usermod -aG adm,cdrom,sudo,plugdev ubuntu
 
-apt-get clean
+apt-get -q=2 clean
 rm /sbin/initctl
 dpkg-divert --local --rename --remove /sbin/initctl
